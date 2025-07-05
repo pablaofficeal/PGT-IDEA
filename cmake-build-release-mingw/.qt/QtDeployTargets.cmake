c08da31c882a6bd6526e228198223f2ffba456c3
pablaofficeal <pavlogorosko56@gmail.com>
2025-07-06 01:02:57 +0200

    delete fix cmake

--- cmake-build-release-mingw/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_untitled20_FILE C:/Users/pavlo/CLionProjects/untitled20/cmake-build-release-mingw/untitled20.exe)
-set(__QT_DEPLOY_TARGET_untitled20_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_untitled20_RUNTIME_DLLS C:/Qt/6.9.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.1/mingw_64/bin/Qt6Core.dll)
